19859d4bb4f9692c36bbde3be625c5c255550167
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2026-05-02 17:17:07 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    // Encodes: "5#Hello5#World"
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for (const string& str : strs) {
+            // Append length + delimiter + content
+            encoded += to_string(str.size()) + "#" + str;
+        }
+        return encoded;
+    }
+
+    // Decodes: "5#Hello5#World"
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+        while (i < s.size()) {
+            // Find the delimiter
+            int delimiter = s.find('#', i); //i denotes start pointer
+            // Get length of the next string
+            int len = stoi(s.substr(i, delimiter));
+            // Extract the string using the length
+            decoded.push_back(s.substr(delimiter + 1, len));
+            // Move index to start of next chunk
+            i = delimiter + 1 + len;
+        }
+        return decoded;
+    }
+};
