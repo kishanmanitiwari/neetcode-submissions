d6622f2302cb1e238779ddd0787ab35048a3d5cd
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2026-07-03 20:03:41 +0530

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // Node found
+
+            // No left child
+            if (root.left == null) {
+                return root.right;
+            }
+
+            // No right child
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // Two children
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
